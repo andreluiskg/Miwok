4bdd37e3c4726ac1259bacd01617930a225dc1d1
andreluiskg <andreluiskg@hotmail.com>
2017-07-08 23:38:04 -0300

    Como o arquivo de layout activity_numbers.xml tornou-se padrão e é utilizado por todas as Activity's, ele foi renomeado para word_list.xml para ficar mais explícito que é genérico e não passar a falsa impressão de exclusividade da classe NumbersActivity.java.   Com a mudança acima os arquivos activity_colors.xml, activity_family.xml e activity_phrases.xml tornaram-se obsoletos, motivo do qual foram excluídos.

--- app/src/main/java/com/andre/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/andre/android/miwok/NumbersActivity.java
@@ -11,7 +11,7 @@ public class NumbersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.word_list);
 
 //        ArrayList<String> words = new ArrayList<String>();
 //        words.add("one");
